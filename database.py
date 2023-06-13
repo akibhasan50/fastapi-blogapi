2c4e96fc4b9e7fa4495aa48b6dd165a450dd21bc
akibhasan50 <tonuakibhasan@gmail.com>
2023-06-14 02:36:15 +0600

    create blog api

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgress:123456@localhost:5432/python_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/python_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,echo= True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
