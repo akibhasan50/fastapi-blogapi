88755c8d11b609838782709765accd99c6781b04
akibhasan50 <tonuakibhasan@gmail.com>
2023-06-15 01:07:34 +0600

    blog delete and update api implemented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import schemas
@@ -16,7 +16,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog",tags=["blog"])
+@app.post("/blog", tags=["blog"], status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     #  new_blog = models.Blog(**request.dist())
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -26,13 +26,36 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog",tags=["blog"])
+@app.get("/blog", tags=["blog"])
 def all_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}",tags=["blog"])
-def single_blog(id: int, db: Session = Depends(get_db)):
+@app.get("/blog/{id}", tags=["blog"], status_code=status.HTTP_200_OK)
+def single_blog(id: int,  db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+               detail=f"Blog with  {id} not found"))
     return blog
+
+
+@app.delete("/blog/{id}", tags=["blog"], status_code=status.HTTP_204_NO_CONTENT)
+def blog_delete(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete()
+    db.commit()
+    return "done"
+
+
+@app.put("/blog/{id}", tags=["blog"], status_code=status.HTTP_202_ACCEPTED)
+def blog_edit(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+   
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+               detail=f"Blog with  {id} not found"))
+    blog.update(
+        {'title': request.title, 'body': request.body})
+    db.commit()
+    return "updated"
