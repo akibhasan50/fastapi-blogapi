e4bd899d447b2d1d55a368751617d7ebd614e1e1
akibhasan50 <tonuakibhasan@gmail.com>
2023-06-14 12:34:53 +0600

    get all blog and single blog api created

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import schemas
 import models
 
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
+app = FastAPI(title='Blog API')
 
 
 def get_db():
@@ -16,10 +16,23 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title = request.title, body = request.body)
+@app.post("/blog",tags=["blog"])
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    #  new_blog = models.Blog(**request.dist())
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog",tags=["blog"])
+def all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}",tags=["blog"])
+def single_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
