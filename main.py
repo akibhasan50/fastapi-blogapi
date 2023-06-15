16e196d82f642b8c3123c2b19f4093e2b56b7db4
akibhasan50 <tonuakibhasan@gmail.com>
2023-06-16 00:43:19 +0600

    user create and get api started

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def all_blog(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{id}", tags=["blog"], status_code=status.HTTP_200_OK)
+@app.get("/blog/{id}", tags=["blog"], status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def single_blog(id: int,  db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -43,14 +43,19 @@ def single_blog(id: int,  db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", tags=["blog"], status_code=status.HTTP_204_NO_CONTENT)
 def blog_delete(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete()
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+               detail=f"Blog with  {id} not found"))
+    blog.delete()
     db.commit()
     return "done"
 
 
 @app.put("/blog/{id}", tags=["blog"], status_code=status.HTTP_202_ACCEPTED)
 def blog_edit(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-   
+
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise (HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -59,3 +64,19 @@ def blog_edit(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
         {'title': request.title, 'body': request.body})
     db.commit()
     return "updated"
+
+
+@app.post("/user", tags=["user"], status_code=status.HTTP_201_CREATED)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user", tags=["user"], status_code=status.HTTP_200_OK)
+def create_user(db: Session = Depends(get_db)):
+    all_user = db.query(models.User).all()
+    return all_user
